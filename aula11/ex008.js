4c74cc79ecfb9f35b19ac330979f6e3696c5de2c
Paulo Gabriel Henrique <84204193+Henrycklol@users.noreply.github.com>
2023-07-12 17:15:49 -0400

    Aula 11 pt.1

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 4.2
+console.log(`A velocidade do seu possante é ${velocidade}`)
+if (velocidade > 60){
+console.log(`passando mais uma marcha`)
+}
+console.log(`tu não é o james da salada de fruta n o fdp`)
---
+++ aula11/ex009.js
@@ -0,0 +1,19 @@
+var pais = 'Polônia'
+console.log(`living in ${pais}`)
+if (pais == 'Nigéria') {
+    console.log(`This is the prince of Nigéria`)
+} else {
+console.log(`I fell scam of prince de merda`)
+}
+
+// ou 
+
+/*
+var faculdade = 'Florestas'
+console.log(`Você faz ${faculdade}`)
+if (faculdade != 'Direito') {
+    console.log(`Você tá de boa`)
+} else {
+console.log(`Já percebeu que a vara do juiz é mto rígida n é mesmo`)
+} 
+*/
