f0ad4dcafe0fce3dc417773cd883653b3634369c
Paulo Gabriel Henrique <84204193+Henrycklol@users.noreply.github.com>
2023-08-06 20:05:09 -0400

    Estruturas de Repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+// estrutura de repetiçao lógica com verificação no início 
+var c = 1
+while (c <= 50) {
+console.log(`vc foi troxa ${c} vez(es)`)
+c++;
+}
+
+//estrutura de repetição lógica com verificação no final
+do{
+    console.log(`vc foi troxa ${c} vez(es)`)
+    c++;
+} while (c <=20)
---
+++ aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('here we go')
+for (var i = 1; i < 10; i++) { 
+    console.log(i)
+}
+console.log('cabo')
